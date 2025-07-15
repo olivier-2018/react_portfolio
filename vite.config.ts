a1f7387365d80a654d4ce834f81b57faff884189
olivier-2018 <olivier-2018@users.noreply.github.com>
2025-07-15 17:35:10 +0200

    cleaned up asset links

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: { // server block only used in Development mode
     host: true,
