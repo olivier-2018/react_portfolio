16b4249ecbaf74ab0c9f219f3ff0d449bfb7aec4
olivier-2018 <olivier-2018@users.noreply.github.com>
2025-07-15 18:11:30 +0200

    moved static assets to docker mounted folder

--- projects_assets/README.md
+++ projects_assets/README.md
@@ -3,14 +3,16 @@ Place your project_pictures and project_movies assets folders here.
 Ex:
 projects_assets
 ├── README.md
+├── hero-portrait.jpg
+├── logo-BTS.gif
 ├── project_movies
 │   ├── demo-video.mp4
 │   ├── drone-inspection.mp4
-│   ├── robot-arm-demo.mp4
-│   └── animation.mp4
+│   └── robot-arm-demo.mp4
 └── project_pictures
     ├── analytics-dashboard.jpg
     ├── cad-optimizer.jpg
     ├── customer-segmentation.jpg
     ├── nav-robot.jpg
     └── vision-drone.jpg
+    
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
+  base: "/", // Set base path for production
+  publicDir: '/public', // Specify the public directory for static assets
   server: { // server block only used in Development mode
     host: true,
     strictPort: true, // Ensure the server uses the specified port
@@ -12,11 +14,7 @@ export default defineConfig(({ mode }) => ({
     },  
     allowedHosts: ['.brontechsolutions.ch']
   },
-  preview: {
-    port: 5173 // Pick your port here
-  },
   plugins: [react()],
-  base: "/", // Set base path for production
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -25,6 +23,9 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: 'dist',
     sourcemap: true
+  },
+  preview: {
+    port: 5173 // Pick your port here
   },
 }));
 
