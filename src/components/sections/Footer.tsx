c0c7537b67444e9c4973740fbd3d6f6f9f94b162
olivier-2018 <olivier-2018@users.noreply.github.com>
2025-07-17 10:47:48 +0200

    updated footer msg

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -3,9 +3,10 @@ import React from "react";
 const lastUpdate = import.meta.env.VITE_GIT_COMMIT_DATE;
 
 export function Footer() {
+  // console.log("Footer component rendered with last update date:", lastUpdate);
   return (
     <footer className="w-full py-4 text-center text-sm text-muted-foreground bg-background border-t border-border mt-12">
-      &copy; {new Date().getFullYear()} Olivier Bron. All rights reserved. (Last updated: {new Date(lastUpdate).toLocaleDateString()}) )  
+      &copy;{new Date().getFullYear()} Olivier Bron. All rights reserved. {lastUpdate && <> Last update: {new Date(lastUpdate).toLocaleDateString()} </>}
     </footer>
   );
 }
